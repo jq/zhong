e5d9173f775cffc5a5f1b4d86a51f1726a53938a
qj <julian.qian@gmail.com>
2010-03-03 10:27:42 +0000

    =second

---
+++ MusicSearch/src/com/trans/music/search/SearchViewWp.java
@@ -0,0 +1,20 @@
+package com.trans.music.search;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SearchViewWp {
+  TextView name;
+  TextView artist;
+  TextView size;
+  TextView album;
+
+  public SearchViewWp(View view) {
+    view.setTag(this);
+    name = (TextView)view.findViewById(R.id.row_title);
+    artist = (TextView)view.findViewById(R.id.row_artist);
+    size = (TextView)view.findViewById(R.id.song_size);
+    album = (TextView) view.findViewById(R.id.row_album);
+  }
+
+}
