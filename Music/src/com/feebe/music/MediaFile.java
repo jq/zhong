185803d6e7907f76f81dbf403984af4fee3b4127
qj <julian.qian@gmail.com>
2010-03-14 06:39:25 +0000

    =add music player

---
+++ Music/src/com/feebe/music/MediaFile.java
@@ -0,0 +1,7 @@
+package com.feebe.music;
+
+public class MediaFile {
+  public static final String UNKNOWN_STRING = "unknown";
+  //"is_podcast" and BOOKMARK
+  // WeekSelector is not easy to add, used in EDIT_PLAYLIST
+}
