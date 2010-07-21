616890c1b6996af8646c26acd62a9da837392429
qj <julian.qian@gmail.com>
2010-07-21 03:37:32 +0000

    =add new adwhirl

--- MusicSearch2/src/com/trans/music/search/BaseList.java
+++ MusicSearch2/src/com/trans/music/search/BaseList.java
@@ -19,7 +19,7 @@ public abstract class BaseList extends ListActivity implements OnItemClickListen
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.list);
-        AdListener.createAds(this);
+        AdListener.createAds(this, R.id.list_main);
         final ListView list = getListView();
         setListAdapter(getAdapter());
         list.setDividerHeight(1);
