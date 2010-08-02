2a56aeb9803d6142a38d8597a470cd9bb4a96eb3
qj <julian.qian@gmail.com>
2010-08-02 10:32:42 +0000

    =util needed by ring

---
+++ util/src/com/util/Utils.java
@@ -0,0 +1,45 @@
+package com.util;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class Utils {
+    
+    private static CharsetEncoder sEncoder = Charset.forName("ISO-8859-1").newEncoder();
+    private static CharsetDecoder sDecoder = Charset.forName("GBK").newDecoder();
+    public static String convertGBK(String input) {
+    	try {
+    		ByteBuffer bbuf = sEncoder.encode(CharBuffer.wrap(input));
+    		CharBuffer cbuf = sDecoder.decode(bbuf);
+    		String output = cbuf.toString();
+    		return output;
+    	} catch (Exception e) {
+    		//e.printStackTrace();
+    		return input;
+    	}
+    }
+}
