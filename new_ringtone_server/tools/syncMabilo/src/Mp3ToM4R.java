c167a708b735dfed10cf24bd002b8b8dd389eac7
moov-xu <moov-xu rabbitxf@gmail.com>
2010-12-28 09:11:11 +0000

    convert mp3 to wav

--- new_ringtone_server/tools/syncMabilo/src/Mp3ToM4R.java
+++ new_ringtone_server/tools/syncMabilo/src/Mp3ToM4R.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import javazoom.jl.converter.Converter;
+import javazoom.jl.converter.Converter.ProgressListener;
+import javazoom.jl.decoder.Header;
+import javazoom.jl.decoder.Obuffer;
 
-public class Mp3ToM4R {
+public class Mp3ToM4R{
 	public static final String GenTempCmd = "mplayer souPath -ao pcm:file=\"tempPath\"";
 	public static final String GenTarCmd = "faac tempPath -o tarPath -w";
 	public static final String RmTempCmd = "rm -rf tempPath";
@@ -19,7 +22,10 @@ public class Mp3ToM4R {
 			//Process proc1 = Runtime.getRuntime().exec(GenTempCmd.replace("souPath", source).replace("tempPath", temp));
 			//Thread.sleep(1000);
 			//if(proc1.exitValue() != 0) return false;
-			new Converter().convert(source, temp);
+			
+			CPListener cpl = new CPListener();
+			new Converter().convert(source, temp, cpl);
+			while (cpl.isNotCompleted());
 			
 			Process proc2 = Runtime.getRuntime().exec(GenTarCmd.replace("tempPath", temp).replace("tarPath", target));
 			Thread.sleep(1000);
@@ -42,5 +48,44 @@ public class Mp3ToM4R {
 		music.setRingName("a.mp3");
 		System.out.println(convert(music));
 	}*/
+	
+}
+
+class CPListener implements ProgressListener {
+	boolean notCompleted = true;
+
+	@Override
+	public boolean converterException(Throwable arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void converterUpdate(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		notCompleted = (arg0 != UPDATE_CONVERT_COMPLETE); 
+	}
+
+	@Override
+	public void decodedFrame(int arg0, Header arg1, Obuffer arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void parsedFrame(int arg0, Header arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readFrame(int arg0, Header arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	boolean isNotCompleted() {
+		return notCompleted;
+	}
 	
 }
