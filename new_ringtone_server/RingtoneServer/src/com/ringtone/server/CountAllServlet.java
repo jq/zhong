a786367632e80cd806fa324fcf762ef3cf20f0ef
huangyang <huangyang001@gmail.com>
2011-01-26 09:29:27 +0000

    add get num of all song api.

--- new_ringtone_server/RingtoneServer/src/com/ringtone/server/CountAllServlet.java
+++ new_ringtone_server/RingtoneServer/src/com/ringtone/server/CountAllServlet.java
@@ -2,45 +2,28 @@ package com.ringtone.server;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+
 
 public class CountAllServlet extends HttpServlet {
 
-	public static final int RESULTS_PER_QUERY = 1000;
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {	
-		if (!DebugUtils.debug) {
-			return;
-		}
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(SongEntry.class);
-		int start = 0;
-		boolean isFinish = false;
-		
-		try {
-			while(!isFinish) {
-				query.setRange(start, start+RESULTS_PER_QUERY);
-				query.addExtension("datanucleus.appengine.datastoreReadConsistency", "EVENTUAL");
-				List<SongEntry> results = (List<SongEntry>) query.execute();
-				DebugUtils.D("Size per request: "+results.size());
-				start += results.size();
-				if (results.size() == 0) {
-					isFinish = true;
-				}
-			}
-		} finally {
-			pm.close();
-		}
-		
-		DebugUtils.D("Total SongEntry: "+start);
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity globalStat = datastore.prepare(new Query("__Stat_Kind__").addFilter("kind_name", FilterOperator.EQUAL, "SongEntry")).asSingleEntity();
+		Long totalSongEntities = (Long) globalStat.getProperty("count");
+		resp.getOutputStream().write(Long.toString(totalSongEntities).getBytes());
+		resp.flushBuffer();
 	}
 	
 }
