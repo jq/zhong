5a95007cb6e1e7b3cee53e4184fd8c9b2764dec3
Zhongchao Yu <zhongchao@gmail.com>
2011-02-27 21:02:17 -0800

    Disable some code path about webview which potentially causes null pointer exception.

--- MusicWizard/src/com/ringtone/music/SogouMusicSearcher.java
+++ MusicWizard/src/com/ringtone/music/SogouMusicSearcher.java
@@ -142,10 +142,14 @@ public class SogouMusicSearcher implements IMusicSearcher {
 		mHandler.post(new Runnable() {
 			@Override
 			public void run() {
+                /*
+                 * The following logic seems to cause NullPointerException.
+                 *
 				if (mWeb != null) {
 					mWeb.stopLoading();
 					mWeb.destroy();
 				}
+                */
 				mWeb = new WebView(context);
 				
 				WebSettings settings = mWeb.getSettings();
